46f6500ee337e0de817e647f8721f4dc6b877429
muhammad-junaid1 <mjunaid.swe@gmail.com>
2023-01-28 16:45:05 +0500

    adding todo item on clicking add button

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {useState} from "react";
+import { useState } from "react";
 import {
   Paper,
   Box,
@@ -10,21 +10,37 @@ import {
   MenuItem,
   Typography,
   Divider,
+  TextField,
+  Button,
 } from "@mui/material";
+import { Edit } from "@mui/icons-material";
 import Logo from "./assets/logo.png";
 
 function App() {
   const [filterValue, setFilterValue] = useState(1);
+  const [inputValue, setInputValue] = useState("");
   const [todoItems, setTodoItems] = useState([]);
 
   const handleChange = (event) => {
     setFilterValue(event.target.value);
-  }
+  };
+
+  const handleInput = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  const handleClick = () => {
+    setTodoItems((todoItems) => {
+      return [...todoItems, inputValue];
+    });
+    setInputValue("");
+  };
+
   return (
     <>
       <Container maxWidth="md">
         <Paper variant="outlined" sx={{ mt: 5, p: 2 }}>
-        {/* Header section */}
+          {/* Header section */}
           <Box>
             <Stack
               direction="row"
@@ -51,14 +67,51 @@ function App() {
                 </FormControl>
               </Box>
             </Stack>
-            <Divider sx={{mt: 3}}/>
+            <Divider sx={{ mt: 3 }} />
           </Box>
 
           {/* Todo Items List */}
           <Box py={4}>
-              {todoItems.length ? todoItems.map((todoItem) => {
+            {todoItems.length ? (
+              todoItems.map((todoItem) => {
                 return <p>{todoItem}</p>;
-              }) : <Typography sx={{color: "red"}} align="center">Nothing yet</Typography>}
+              })
+            ) : (
+              <Typography sx={{ color: "red" }} align="center">
+                Nothing yet
+              </Typography>
+            )}
+          </Box>
+
+          <Box sx={{ background: "#de6873", borderRadius: 1 }} py={1} px={2}>
+            <Stack
+              alignItems="center"
+              justifyContent="space-between"
+              direction="row"
+            >
+              <Edit sx={{ color: "white" }} />
+              <TextField
+                onInput={handleInput}
+                variant="outlined"
+                InputProps={{ disableUnderline: true }}
+                fullWidth
+                value={inputValue}
+                sx={{
+                  "& .MuiOutlinedInput-root.Mui-focused": {
+                    "& > fieldset": {
+                      borderColor: "red",
+                    },
+                  },
+                  background: "white",
+                  ml: 3,
+                  mr: 2,
+                  borderRadius: 1,
+                }}
+              />
+              <Button onClick={handleClick} variant="contained" color="secondary" sx={{py: 1.5}}>
+                Add
+              </Button>
+            </Stack>
           </Box>
         </Paper>
       </Container>
